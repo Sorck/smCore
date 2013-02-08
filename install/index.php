94aa07bfb6d072644de7b4c2796a74d927079471
Sorck <colonel.sorck@gmail.com>
2013-02-08 18:39:41 -0500

    Directory changes

---
+++ install/index.php
@@ -0,0 +1,104 @@
+<?php
+
+/**
+ * smCore Installation script.
+ *
+ * Sets up the database if settings.php is correctly configured.
+ *
+ * @package smCore
+ * @author smCore Dev Team
+ * @license MPL 1.1
+ * @version 1.0 Alpha
+ *
+ * The contents of this file are subject to the Mozilla Public License Version 1.1
+ * (the "License"); you may not use this package except in compliance with the
+ * License. You may obtain a copy of the License at http://www.mozilla.org/MPL/
+ *
+ * The Original Code is smCore.
+ *
+ * The Initial Developer of the Original Code is the smCore project.
+ *
+ * Portions created by the Initial Developer are Copyright (C) 2013
+ * the Initial Developer. All Rights Reserved.
+ *
+ * @todo Move up to the main directory
+ * @todo Use smCore's database libraries
+ * @todo Actually perform the database query
+ * @todo Create the settings file
+ * @todo Remove the /other directory
+ */
+
+// Register the autoloader
+require_once(dirname(__DIR__) . '/library/smCore/Autoloader.php');
+new smCore\Autoloader(null, dirname(__DIR__) . '/library');
+
+echo '<!doctype html>
+<head>
+<title>smCore installation</title>
+</head>
+<body>
+<h1>This is pre-alpha software and is for development purposes only!</h1>';
+
+// Does the settings file exist?
+if (!file_exists('settings.php'))
+{
+    // Try building a settings file
+	$protocol = 'http'.(!empty($_SERVER['HTTPS']) ? 's' : '');
+	$url = $protocol.'://'.$_SERVER['HTTP_HOST'] . rtrim(str_replace(str_replace(array(__DIR__, '\\', '/'), '', __FILE__), '', $_SERVER['SCRIPT_NAME']), '/');
+	$settings = file_get_contents(__DIR__ . '/settings.php');
+	$settings = str_replace(array(
+		'/home/my_site/public_html',
+		'http://www.youdidntchangeyoursettingsfile.lol',
+		'smcore.mysite.com',
+		'noreply@mysite.com',
+	),
+	array(
+		rtrim(dirname(__DIR__), '\\/'),
+		$url,
+		$_SERVER['HTTP_HOST'],
+		'noreply@' . $_SERVER['HTTP_HOST'],
+	), $settings);
+	file_put_contents('settings.php', $settings);
+	echo 'Settings file created.';
+}
+
+// Load up the settings
+require('settings.php');
+$settings = new Settings;
+
+// Has the URL been changed?
+if ($settings['url'] == 'http://www.youdidntchangeyoursettingsfile.lol')
+{
+    die('You have not correctly set your URL in ' . dirname(__DIR__) . '/settings.php');
+}
+
+// Connect to the database
+$con = mysql_connect($settings['database']['host'], $settings['database']['user'], $settings['database']['password']);
+if (!$con)
+{
+	echo '<p>Please add your database settings to the settings file.</p>';
+}
+else
+{
+	$qry = file_get_contents('other/database.sql');
+	$fixedqry = str_replace('{db_prefix}', $settings['database']['dbname'].'`.`'.$settings['database']['prefix'], $qry);
+    $queries = explode(';', $fixedqry);
+    do {
+        foreach ($queries as $query)
+        {
+            mysql_query($query);
+            if ($error = mysql_error())
+            {
+                echo "<p>", $error, "</p>
+                <p>MySQL error, run the following query in PHPMyAdmin:</p>\n<textarea>" , $fixedqry, '</textarea>';
+                break 2;
+            }
+        }
+    	echo '<p>Database should have been installed.</p>';
+        echo '<a href="/">Visit your newly installed smCore site.</a>';
+    } while (true);
+}
+
+echo '
+</body>
+</html>';
---
+++ install/settings.php
@@ -0,0 +1,122 @@
+<?php
+
+/**
+ * smCore Settings
+ *
+ * Contains all of your smCore settings.
+ *
+ * @package smCore
+ * @author smCore Dev Team
+ * @license MPL 1.1
+ * @version 1.0 Alpha
+ *
+ * The contents of this file are subject to the Mozilla Public License Version 1.1
+ * (the "License"); you may not use this package except in compliance with the
+ * License. You may obtain a copy of the License at http://www.mozilla.org/MPL/
+ *
+ * The Original Code is smCore.
+ *
+ * The Initial Developer of the Original Code is the smCore project.
+ *
+ * Portions created by the Initial Developer are Copyright (C) 2013
+ * the Initial Developer. All Rights Reserved.
+ */
+
+class Settings extends smCore\Settings
+{
+	protected $_settings = array(
+		/**
+		 * These values should not include trailing slashes.
+		 *
+		 * PATH       Path where index.php is located
+		 * MODULE_DIR Path to the /modules/ directory
+		 * THEME_DIR  Path to the /themes/ directory
+		 * CACHE_DIR  File cache directory, used by the Twig template cache and the File cache if enabled
+		 */
+		'path'              => '/home/my_site/public_html',
+		'module_dir'        => '/home/my_site/public_html/modules',
+		'theme_dir'         => '/home/my_site/public_html/themes',
+		'cache_dir'         => '/home/my_site/public_html/cache',
+
+		/**
+		 * Again, no slashes at the end - http://www.example.com/smcore *not* http://www.example.com/smcore/
+		 */
+		'url'               => 'http://www.youdidntchangeyoursettingsfile.lol',
+
+		/**
+		 * You'll probably only need to configure COOKIE_DOMAIN
+		 */
+		'cookie_path'       => '/',
+		'cookie_name'       => 'smcore_login',
+		'cookie_domain'     => 'smcore.mysite.com',
+
+		/**
+		 * Use database-driven sessions instead of file-based sessions?
+		 */
+		'session_driver'    => 'Database',
+
+		/**
+		 * @todo: Default time zone should be a database setting and only used to display times, not store them
+		 */
+		'timezone'          => 'America/Los_Angeles',
+
+		/**
+		 * This string is used so multiple smCore installations don't interfere with each other
+		 */
+		'site_key'          => '07h8fAN4',
+	
+		'default_lang'      => 'en_US',
+		'default_theme'     => 1,
+
+		'mail_from'         => 'noreply@mysite.com',
+		'mail_from_name'    => 'smCore',
+
+		/**
+		 * Database connection parameters
+		 *
+		 * Available drivers: PDOMySql
+		 *
+		 * Options:
+		 *     PDOMySQL:
+		 *         host     string Hostname of the MySQL server, usually localhost
+		 *         user     string Your MySQL username
+		 *         password string Your MySQL password
+		 *         dbname   string The name of the database to connect to
+		 *     All:
+		 *         prefix   string Prefix all table names with this string, to prevent clashes with other software or other smCore installations
+		 */
+		'database' => array(
+			'driver'   => 'PDOMySql',
+			'host'     => 'localhost',
+			'user'     => '',
+			'password' => '',
+			'dbname'   => '',
+			'prefix'   => 'smcore_',
+		),
+
+		/**
+		 * Caching mechanism parameters
+		 *
+		 * Available drivers: Memcached, Blackhole, APC, File
+		 *
+		 * Options:
+		 *     APC:
+		 *         (none)
+		 *     Blackhole:
+		 *         (none)
+		 *     File:
+		 *         directory       string  
+		 *     Memcached:
+		 *         servers         array   Array of servers, keys are "host", "port", "weight"
+		 *         persistent      boolean Utilize a persistent connection to the Memcached server
+		 *         connect_timeout int     Time to wait before considering a connection failed
+		 *         retry_timeout   int     Time to wait before attempting to retry an operation
+		 */
+		'cache' =>  array(
+			'driver'      => 'Blackhole',
+			'default_ttl' => 3600,
+		),
+
+		'debug' => true,
+	);
+}
