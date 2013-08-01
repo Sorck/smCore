9c31e5a49c0d2d9657ab9203bb9e4b8ab988eb68
Sorck <colonel.sorck@gmail.com>
2013-08-01 19:59:43 -0400

    Needing the Composer autoloader

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 
 ini_set('display_errors', 1);
 
+// Load the composer autoloader
+require_once 'library/autoload.php';
+
 // Register the autoloader
 include_once __DIR__ . '/library/smCore/Autoloader.php';
 new smCore\Autoloader(null, __DIR__ . '/library');
