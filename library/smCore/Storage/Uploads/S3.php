1bc51c8dd9b7ea07ec4497ba0eae8cc7c5ed0d9f
Sorck <colonel.sorck@gmail.com>
2013-08-03 19:02:45 -0400

    Begun interacing with the S3 SDK.

--- library/smCore/Storage/Uploads/S3.php
+++ library/smCore/Storage/Uploads/S3.php
@@ -23,22 +23,57 @@
 
 namespace smCore\Storage\Uploads;
 
-use smCore\Model\Upload, smCore\Storage\Uploads\AbstractUploads, smCore\Storage\AbstractStorage;
+use smCore\Model\Upload, smCore\Storage\Uploads\AbstractUploads, smCore\Storage\AbstractStorage, smCore\Application,
+Aws\S3\S3Client;
 
 class S3 extends AbstractUploads
 {
+    /**
+     * @var s3client The Amazon S3 client object that we're going to use.
+     */
+    protected $_s3client;
+    
+    /**
+     * @method __construct This creates the s3client object that we will be needing.
+     * @todo This object might be better being lazy loaded.
+     * @todo If AWS is used elsewhere it would be better for us to use the service builder.
+     */
+    public function __construct(Application $app)
+    {
+        parent::__construct($app);
+        $this->_s3client = S3Client::factory(array(
+            'key'    => $app['settings']['aws']['key_id'],
+            'secret' => $app['settings']['aws']['secret_key'],
+        ));
+    }
+    
     public function get($uid)
     {
         throw new Exception('Not implemented.');
     }
     
+    /**
+     * @todo Do we delete using the UID or the actual filename?
+     */
     public function delete($uid)
     {
         throw new Exception('Not implemented.');
+        $this->_s3client->deleteObject(array(
+            'bucket' => $this->_app['settings']['aws']['bucket'],
+            'key' => $uid
+        ));
+        // @todo Clean the database
     }
     
     public function save(Upload $file)
     {
         throw new Exception('Not implemented.');
+        $this->_s3client->putObject(array(
+            'bucket' => $this->_app['settings']['aws']['bucket'],
+            'key' => $file->uid,
+            'ACL' => 'public-read',
+            'Body' => fopen($file['location'])
+        ));
+        // @todo Add to the database.
     }
 }
